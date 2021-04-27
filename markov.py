dc09760c256156aaeed7bdadc7d03b3f7da2d03c
Mateusz Kopij <sagasu@gmail.com>
2021-04-27 19:50:52 +0100

    basic coin toss game simulation

---
+++ markov.py
@@ -0,0 +1,22 @@
+import random
+
+""" 
+This is a gamblers dilemma problem
+we have 10 pounds, and we toss a coin, each time we toss head we gain 1 pound, else we loose 1 pound
+we are checking if we run out of money, and when
+"""
+
+def get_count_before_run_out_of_money():
+    money, count = 10, 0
+    while money:
+        toss = random.random()
+        if toss < .50:
+            money -= 1
+        if toss > .50:
+            money += 1
+        count += 1
+    return count
+
+sims = [get_count_before_run_out_of_money() for _ in range(100)]
+
+print(sims)
